d23e29999edf1362e05661188c5e8acd5947ed93
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2022-07-21 01:39:13 +0200

    implementing the create user response and user request model

--- src/main/java/com/appsdeveloperblog/app/we/ui/controller/UserController.java
+++ src/main/java/com/appsdeveloperblog/app/we/ui/controller/UserController.java
@@ -4,8 +4,12 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.appsdeveloperblog.app.we.ui.model.request.UserDetailsRequestModel;
+import com.appsdeveloperblog.app.we.ui.model.response.UserRest;
 
 @RestController
 @RequestMapping("/users")
@@ -17,8 +21,8 @@ public class UserController {
 	}
 	
 	@PostMapping
-	public String createUser() {		
-		return "create user was called";
+	public UserRest createUser(@RequestBody UserDetailsRequestModel userDetails) {		
+		return null;
 	}
 	
 	@PutMapping

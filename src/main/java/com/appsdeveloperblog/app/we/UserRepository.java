afe199016aee4b87063d5ae579dc636501743dbd
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2022-07-21 05:52:12 +0200

    implementing userrepository class

---
+++ src/main/java/com/appsdeveloperblog/app/we/UserRepository.java
@@ -0,0 +1,11 @@
+package com.appsdeveloperblog.app.we;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.appsdeveloperblog.app.we.io.entity.UserEntity;
+
+@Repository
+public interface UserRepository extends CrudRepository<UserEntity, Long> {
+
+}

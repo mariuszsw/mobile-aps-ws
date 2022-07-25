c9afd917b4ab55d71df55c9e8781b5c8cd201b3f
Mariusz Świerkot <mariusz.swierkot@gmail.com>
2022-07-25 05:19:38 +0200

    encrypt user password

--- src/main/java/com/appsdeveloperblog/app/we/MobileAppWsApplication.java
+++ src/main/java/com/appsdeveloperblog/app/we/MobileAppWsApplication.java
@@ -2,6 +2,9 @@ package com.appsdeveloperblog.app.we;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class MobileAppWsApplication {
@@ -9,5 +12,10 @@ public class MobileAppWsApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(MobileAppWsApplication.class, args);
 	}
+
+    @Bean
+    BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
 }
--- src/main/java/com/appsdeveloperblog/app/we/impl/UserServiceImpl.java
+++ src/main/java/com/appsdeveloperblog/app/we/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package com.appsdeveloperblog.app.we.impl;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import com.appsdeveloperblog.app.we.UserRepository;
@@ -15,10 +16,12 @@ public class UserServiceImpl implements UserService {
 
 	@Autowired
 	UserRepository userRepository;
-
 	
 	@Autowired
 	Utils utils;
+	
+	@Autowired
+	BCryptPasswordEncoder bCryptPasswordEncoder;
 	
 	@Override
 	public UserDto createUser(UserDto user) {
@@ -30,7 +33,7 @@ public class UserServiceImpl implements UserService {
 
 		String publicUserId = utils.generateUserId(30);
 		userEntity.setUserId(publicUserId);
-		userEntity.setEncryptedPassword("test");
+		userEntity.setEncryptedPassword(bCryptPasswordEncoder.encode(user.getPassword()));
 
 		UserEntity storedUserDetails = userRepository.save(userEntity);
 
